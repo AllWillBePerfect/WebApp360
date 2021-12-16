2d5720fb29c669166a57693243393e34f27e6d66
AllWillBePerfect <reserveproject@mail.ru>
2021-12-16 20:14:32 +0300

    firts step for our dream

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const exphbs = require("express-handlebars")
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
 const app = express()
 
-const PORT = process.env.PORT || 80
 
-app.get('/', (req, res) => {
-    res.end('Title')
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: 'hbs'
 })
 
-app.get('/about', (req, res) => {
-    res.end('<h1>About page</h1>')
-})
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.listen(PORT, () => {
-    console.log('Server has been started...')
-})
-//hello
-//my best friend
+app.use(todoRoutes)
+
+async function start () {
+    try {
+        await mongoose.connect('mongodb+srv://user:12341234@cluster0.5vx5r.mongodb.net/myFirstDatabase', {
+            useNewUrlParser: true
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
+
+
